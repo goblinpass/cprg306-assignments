c27ebc337e67b54261907dd86c855bf250a13a8f
goblinpass <119914614+goblinpass@users.noreply.github.com>
2023-11-08 20:26:07 -0700

    commit

--- app/week8/_utils/firebase.js
+++ app/week8/_utils/firebase.js
@@ -1,21 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+import { getAuth } from "firebase/auth";
+ 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAoqnQUNfp3KlOCoVuW8Y8qCyR38_vliF8",
-  authDomain: "cprg306-assignments-c7ead.firebaseapp.com",
-  projectId: "cprg306-assignments-c7ead",
-  storageBucket: "cprg306-assignments-c7ead.appspot.com",
-  messagingSenderId: "652292916906",
-  appId: "1:652292916906:web:64cda0941374a2a624f2d3",
-  measurementId: "G-747F9BPZ7V"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
+ 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
